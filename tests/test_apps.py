ac3f49f1418a0dc135e30e4c54ba64a6dcd32666
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2025-01-27 07:25:13 +0100

    Autodiscover()-Fix

--- tests/test_apps.py
+++ tests/test_apps.py
@@ -1,4 +1,11 @@
+import sys
+from pathlib import Path
+from unittest import mock
+
 from django.apps import AppConfig, apps
+
+from queuebie import MessageRegistry
+from queuebie.apps import QueuebieConfig
 
 
 def test_app_config():
@@ -7,3 +14,12 @@ def test_app_config():
     assert isinstance(app_config, AppConfig)
     assert app_config.default_auto_field == "django.db.models.BigAutoField"
     assert app_config.name == "queuebie"
+
+
+@mock.patch.object(MessageRegistry, "autodiscover")
+def test_app_ready_autodiscover_called(mocked_autodiscover):
+    config = QueuebieConfig(app_name="queuebie", app_module=sys.modules[__name__])
+    config.path = str(Path(__file__).resolve().parent)
+    config.ready()
+
+    assert mocked_autodiscover.call_count == 1
